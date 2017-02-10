264c5e419caa2394de176e44e3744c95c89f21fd
Stephen J Day <stephen.day@docker.com>
2017-02-09 18:25:33 -0800

    api/services/container: define container service
    After receiving feedback, it was found that we had to rethink the
    dependence of a filesystem for containerd. This is a first pass at
    redefining the request in this vein.
    
    Specifically, we now take an opaque specification, along with a runtime
    and mounts. The runtime will dispatch to execution backends, such as
    runc. The specification is specific to the runtime, so the user still
    gets access to the direct contents of the runc configuration.
    
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

---
+++ api/services/container/gen.go
@@ -0,0 +1,3 @@
+package execution
+
+//go:generate protoc -I.:../../../vendor:../../../vendor/github.com/gogo/protobuf:../../../../../..:/usr/local/include --gogoctrd_out=plugins=grpc,import_path=github.com/docker/containerd/api/execution,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:. container.proto
